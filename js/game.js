188f7e5ed74f03e9b5610e0b5af738b12b720811
inesscatana <ines.arts@hotmail.com>
2024-09-28 19:20:01 +0100

    Move the player

--- js/game.js
+++ js/game.js
@@ -28,26 +28,97 @@ class Player {
 	}
 
 	draw() {
+		ctx.save()
+
+		ctx.translate(
+			player.position.x + player.width / 2,
+			player.position.y + player.height / 2
+		)
+
+		ctx.rotate(this.rotation)
+
+		ctx.translate(
+			-player.position.x - player.width / 2,
+			-player.position.y - player.height / 2
+		)
+
+		ctx.drawImage(
+			this.image,
+			this.position.x,
+			this.position.y,
+			this.width,
+			this.height
+		)
+
+		ctx.restore()
+	}
+	update() {
 		if (this.image) {
-			ctx.drawImage(
-				this.image,
-				this.position.x,
-				this.position.y,
-				this.width,
-				this.height
-			)
+			this.draw()
+			this.position.x += this.velocity.x
 		}
 	}
 }
 
 const player = new Player()
+const keys = {
+	ArrowLeft: {
+		pressed: false,
+	},
+	ArrowRight: {
+		pressed: false,
+	},
+	space: {
+		pressed: false,
+	},
+}
 
 function animate() {
 	requestAnimationFrame(animate)
 	ctx.fillStyle = 'black'
 	ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-	player.draw()
+	player.update()
+
+	if (keys.ArrowLeft.pressed && player.position.x >= 0) {
+		player.velocity.x = -7
+		player.rotation = -0.15
+	} else if (
+		keys.ArrowRight.pressed &&
+		player.position.x + player.width <= canvas.width
+	) {
+		player.velocity.x = 7
+		player.rotation = 0.15
+	} else {
+		player.velocity.x = 0
+		player.rotation = 0
+	}
 }
 
 animate()
+
+addEventListener('keydown', ({ key }) => {
+	switch (key) {
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = true
+			break
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = true
+			break
+		case ' ':
+			break
+	}
+})
+
+addEventListener('keyup', ({ key }) => {
+	switch (key) {
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = false
+			break
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = false
+			break
+		case ' ':
+			break
+	}
+})
