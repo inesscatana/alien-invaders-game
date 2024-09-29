d972988674445c4dc74b637c545650bef1a1baf6
inesscatana <ines.arts@hotmail.com>
2024-09-29 12:30:34 +0100

    Create and move grids of invaders

--- js/game.js
+++ js/game.js
@@ -88,12 +88,13 @@ class Projectile {
 }
 
 class Invader {
-	constructor() {
+	constructor({ position }) {
 		this.velocity = {
 			x: 0,
 			y: 0,
 		}
-		this.position = { x: canvas.width / 2, y: canvas.height }
+
+		this.position = position
 		this.loadImage()
 	}
 
@@ -106,8 +107,8 @@ class Invader {
 			this.width = image.width * scale
 			this.height = image.height * scale
 			this.position = {
-				x: canvas.width / 2 - this.width / 2,
-				y: canvas.height / 2,
+				x: this.position.x,
+				y: this.position.y,
 			}
 		}
 	}
@@ -122,18 +123,65 @@ class Invader {
 		)
 	}
 
-	update() {
+	update({ velocity }) {
 		if (this.image) {
 			this.draw()
-			this.position.x += this.velocity.x
-			this.position.y += this.velocity.y
+			this.position.x += velocity.x
+			this.position.y += velocity.y
+		}
+	}
+}
+
+class Grid {
+	constructor() {
+		this.position = {
+			x: 0,
+			y: 0,
+		}
+
+		this.velocity = {
+			x: 3,
+			y: 0,
+		}
+
+		this.invaders = []
+
+		const columns = Math.floor(Math.random() * 10 + 5)
+		const rows = Math.floor(Math.random() * 5 + 2)
+
+		this.width = columns * 30
+
+		for (let x = 0; x < columns; x++) {
+			for (let y = 0; y < rows; y++) {
+				this.invaders.push(
+					new Invader({
+						position: {
+							x: x * 30,
+							y: y * 30,
+						},
+					})
+				)
+			}
+		}
+	}
+
+	update() {
+		this.position.x += this.velocity.x
+		this.position.y += this.velocity.y
+
+		this.velocity.y = 0
+
+		if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+			this.velocity.x = -this.velocity.x
+			this.velocity.y = 30
 		}
 	}
 }
 
 const player = new Player()
 const projectiles = []
-const invader = new Invader()
+const grids = [new Grid()]
+
 const keys = {
 	ArrowLeft: false,
 	ArrowRight: false,
@@ -171,9 +219,16 @@ function animate() {
 	ctx.fillStyle = 'black'
 	ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-	invader.update()
 	player.update()
 	handleProjectiles()
+
+	grids.forEach((grid) => {
+		grid.update()
+		grid.invaders.forEach((invader) => {
+			invader.update({ velocity: grid.velocity })
+		})
+	})
+
 	handlePlayerMovement()
 }
 
